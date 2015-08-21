354ece1c78703a44e4f1da4bac4bcf42aa16e4ae
Saskia Heckele <saskia.heckele@pentasys.de>
2015-08-21 07:17:12 +0200

    Modified Last Test

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -25,8 +25,9 @@ $(function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
-        
-        // loops through each feed and ensures that a url is definden and not empty
+
+		// loops through each feed and ensures that a url is
+		// definden and not empty
         it('have url', function() {
             allFeeds.forEach(function(feed){
                 expect(feed.url).toBeDefined();
@@ -34,7 +35,8 @@ $(function() {
             });
         });
 
-    // loops through each feed and ensures that a name is definden and not empty
+		// loops through each feed and ensures that a name is
+		// definden and not empty
         it('have name', function() {
             allFeeds.forEach(function(feed){
                 expect(feed.name).toBeDefined();
@@ -42,54 +44,60 @@ $(function() {
             });
         });
     });
-    
-    
+
+
     describe('The Menu', function() {
         // Checks, if the class "menu-hidden" is inside the body
         it('is hidden by default', function() {
             expect($('body').hasClass("menu-hidden")).toBe(true);
         });
-        
-        // Checks that the menu reacts correctly when the icon is clicked 
+
+        // Checks that the menu reacts correctly when the icon is clicked
         it('is displayed and hidden correctly', function() {
             // Triggers a click on the element with the class menu-icon-link
             $('.menu-icon-link').trigger('click');
-            // through the click the menu has to be displayed. This means, the Class menu-hidden should not be in the body.
+			// through the click the menu has to be displayed. This
+			// means, the Class menu-hidden should not be in the body.
             expect($('body').hasClass("menu-hidden")).not.toBe(true);
             // Triggers a new click on the class menu-icon-link
             $('.menu-icon-link').trigger('click');
-            // now the menu should be hidden again. the class menu-hidden should be in the body again
+			// now the menu should be hidden again. the class
+			// menu-hidden should be in the body again
             expect($('body').hasClass("menu-hidden")).toBe(true);
         });
     });
-    
-    
+
+
     describe('Initial Entries', function() {
 
-         // calls before the test the function loadFeed with the jasmine function done() as callback. So jasmine knows, when the async loading is finished.
+		// calls before the test the function loadFeed with the
+		// jasmine function done() as callback. So jasmine knows, when
+		// the async loading is finished.
         beforeEach(function(done) {
             loadFeed(0, function(){
                 done();
             });
         });
 
-        // after the initial loading is completed it checks if a entry inside the class feed is existing.
+		// after the initial loading is completed it checks if a
+		// entry inside the class feed is existing.
         it('are loaded', function(done) {
             var feedHasEntrie = $('.feed').has('.entry-link');
             expect(feedHasEntrie.length).toBeGreaterThan(0);
-            done();	
+            done();
         });
-
     });
-    
-    
+
+
     describe('New Feed Selection', function() {
 
         // this test ensures, that a new feed is actually loaded
         var entriesBeforeLoad;
         var entriesAfterLoad;
 
-        // calls before the test the function loadFeed with the jasmine function done() as callback. So jasmine knows, when the async loading is finished.
+		// calls before the test the function loadFeed with the
+		// jasmine function done() as callback. So jasmine knows, when
+		// the async loading is finished.
         beforeEach(function(done) {
             // empties the feed so that no prior content is existing
             $('.feed').empty();
@@ -98,19 +106,19 @@ $(function() {
                 // saves the content of the first feed
                 entriesBeforeLoad = $('.feed').html();
                 // loads the second feed
+
                 loadFeed(1, function(){
                     // saves the content of the second feed
                     entriesAfterLoad = $('.feed').html();
                     done();
                 });
-                done();
             });
         });
-         
+
         it('changes are working', function() {
             // matches the two saved contents. they shouldn't be equal.
             expect(entriesBeforeLoad).not.toEqual(entriesAfterLoad);
         });
     });
-       
+
 }());
